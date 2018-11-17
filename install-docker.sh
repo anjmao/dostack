9ad26a9438d030dcd3375431cbb15be438687882
Andzej Maciusovic <anjmao@Andzejs-iMac.lan>
2018-11-17 22:37:21 +0200

    initil scripts

---
+++ install-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo snap install docker
+sudo groupadd docker
+sudo usermod -aG docker anjmao
---
+++ setup-ssh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# generate public key on you machine
+ssh-keygen -t rsa
+
+# copy it to VM
+ssh-copy-id <name>@<ip>
+
+# inside vm change ssh config
+
+sudo vim /etc/ssh/sshd_config
+# 1. Change Port
+# 2. Set PasswordAuthentication no
+
+
